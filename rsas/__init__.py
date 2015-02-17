21b023dbe71624fbf2023cd60a278257e1394304
Dano Wilusz <dwilusz1@jhu.edu>
2015-02-16 22:03:18 -0400

    Function to simulate 1st order reaction
    The following changes have been made:
    - added function to _util.py that simulates first order reactions
    - added function to __init__.py.
    - created example that runs function called
    lower_hafren_1st_order_reaction
    - created datafile to accompany example, with Mg data
    
    Still to do:
    - add optimization function

--- rsas/__init__.py
+++ rsas/__init__.py
@@ -2,3 +2,4 @@ from _rsas import solve
 from _rsas_functions import create_function
 from _util import transport
 from _util import transport_with_evapoconcentration
+from _util import transport_with_evapoconcentration_1st_order_reaction
