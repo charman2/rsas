444ef003c106124a31bc28e14b2ded5a688c8320
Ciaran Harman <ciaran.harman@gmail.com>
2015-06-22 17:39:41 -0400

    Added simple decay convolution function

--- rsas/__init__.py
+++ rsas/__init__.py
@@ -3,3 +3,4 @@ from _rsas_functions import create_function
 from _util import transport
 from _util import transport_with_evapoconcentration
 from _util import transport_with_evapoconcentration_1st_order_reaction
+from _util import transport_with_decay
